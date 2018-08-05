c33a00ee2f27024889b80111b90e09cf8e91ff95
Куксенков Андрей <neo.qlive@gmail.com>
2018-08-05 15:40:14 +0300

    Добавьте файлы проекта.

---
+++ WindowsFormsTest/MainForm.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace WindowsFormsTest
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tbInput = new System.Windows.Forms.TextBox();
+            this.label = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // tbInput
+            // 
+            this.tbInput.Location = new System.Drawing.Point(12, 12);
+            this.tbInput.Multiline = true;
+            this.tbInput.Name = "tbInput";
+            this.tbInput.Size = new System.Drawing.Size(175, 78);
+            this.tbInput.TabIndex = 0;
+            this.tbInput.Text = "Hello";
+            // 
+            // label
+            // 
+            this.label.AutoSize = true;
+            this.label.Location = new System.Drawing.Point(12, 112);
+            this.label.Name = "label";
+            this.label.Size = new System.Drawing.Size(0, 13);
+            this.label.TabIndex = 1;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(349, 137);
+            this.Controls.Add(this.label);
+            this.Controls.Add(this.tbInput);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "MainForm";
+            this.Text = "Form1";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.MainForm_Paint);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbInput;
+        private System.Windows.Forms.Label label;
+    }
+}
+
---
+++ WindowsFormsTest/MainForm.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Drawing;
+using System.Timers;
+using System.Windows.Forms;
+
+namespace WindowsFormsTest
+{
+    public partial class MainForm : Form
+    {
+        private System.Timers.Timer _timer;
+        private System.Timers.Timer _timerDrawBorder;
+        private bool _changeBorder;
+
+        public MainForm()
+        {
+            InitializeComponent();
+            InitializeTimers();
+
+            MouseDown += OnMouseDown;
+            MouseUp += OnMouseUp;
+        }
+
+        private void InitializeTimers()
+        {
+            // Таймер для тика нажатой кнопки мышки.
+            _timer = new System.Timers.Timer
+            {
+                Interval = 500,
+                Enabled = false,
+                AutoReset = true
+            };
+            _timer.Elapsed += OnTimerElapsed;
+
+            // Таймер для кратковременной отрисовки рамки
+            _timerDrawBorder = new System.Timers.Timer
+            {
+                Interval = 100,
+                Enabled = false,
+                AutoReset = true
+            };
+            _timerDrawBorder.Elapsed += OnTimerDrawBorderElapsed;
+        }
+
+        private void OnTimerElapsed(Object source, ElapsedEventArgs e)
+        {
+            _changeBorder = true;
+
+            // Выполняем в потоке диспатчера.
+            Invoke((MethodInvoker)delegate
+            {
+                if (string.IsNullOrWhiteSpace(tbInput.Text))
+                {
+                    Clipboard.SetText(tbInput.Text, TextDataFormat.UnicodeText);
+                }
+
+                label.Text = "Скопировано в буфер обмена";
+
+                Refresh();
+            });
+
+            _timer.Stop();
+            _timerDrawBorder.Enabled = true;
+        }
+
+        private void OnTimerDrawBorderElapsed(Object source, ElapsedEventArgs e)
+        {
+            _changeBorder = false;
+
+            // Выполняем в потоке диспатчера.
+            Invoke((MethodInvoker)delegate
+            {
+                label.Text = string.Empty;
+
+                Refresh();
+            });
+
+            _timerDrawBorder.Stop();
+        }
+
+        private void OnMouseDown(object sender, MouseEventArgs e)
+        {
+            _timer.Enabled = true;
+        }
+
+        private void OnMouseUp(object sender, MouseEventArgs e)
+        {
+            _timer.Enabled = false;
+            _timerDrawBorder.Enabled = false;
+
+            // Сбрасываем рамку, если отжали кнопку раньше.
+            _changeBorder = false;
+            Refresh();
+        }
+
+        private void MainForm_Paint(object sender, PaintEventArgs e)
+        {
+            ChangeBorder(e, Color.Red);
+        }
+
+        private void ChangeBorder(PaintEventArgs e, Color color)
+        {
+            if (_changeBorder)
+            {
+                ControlPaint.DrawBorder(e.Graphics, e.ClipRectangle,
+                      Color.Red, 2, ButtonBorderStyle.Solid,
+                      Color.Red, 2, ButtonBorderStyle.Solid,
+                      Color.Red, 2, ButtonBorderStyle.Solid,
+                      Color.Red, 2, ButtonBorderStyle.Solid);
+            }
+        }
+    }
+}
+
